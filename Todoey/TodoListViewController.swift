960fd54ddcee9c5effa90b4cde479595f09b3f6a
Shiroshana Tissera <shiroshanatissera@shiroshanas-mbp>
2018-12-27 12:19:24 +0000

    Adding user defaults and persist storing

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,10 +12,15 @@ class TodoListViewController: UITableViewController {
   
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy  Demogorgon"]
     
+    let defaults = UserDefaults.standard
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+             itemArray = items
+        }
     }
 
     
@@ -68,6 +73,9 @@ class TodoListViewController: UITableViewController {
             //What will happen once the user clicks the add item button on UI alert
             
             self.itemArray.append(textField.text!)
+            
+           self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
             print("Success!")
         }
